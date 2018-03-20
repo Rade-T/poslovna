20294446dcb459da0a8bf29898788c00d04fc75e
Marko <marko46va@gmail.com>
2018-03-20 13:35:38 +0100

    update

--- src/main/java/pi/model/Otpremnica.java
+++ src/main/java/pi/model/Otpremnica.java
@@ -1,5 +1,45 @@
 package pi.model;
 
-public class Otpremnica {
+import java.util.Date;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class Otpremnica {
+	
+	@Id
+	@Column(nullable = false, unique = true)
+	public int brojOtpremnice;
+	
+	@Column
+	public Date datumOtpremnice;
+
+	@Column(nullable = false, precision = 15)
+	public float osnovica;
+	
+	@Column(nullable = false, precision = 15)
+	public float ukupanPdv;
+	
+	@Column(nullable = false, precision = 15)
+	public float iznosZaPlacanje;
+	
+	/*
+	@ManyToOne
+	public PoslovniPartner poslovniPartner;
+	
+	@OneToMany(mappedBy="otpremnica")
+	public List<IzlaznaFaktura> izlaznaFaktura;
+	
+	@ManyToOne
+	public PoslovnaGodina poslovnaGodina;
+	
+	@OneToMany(mappedBy="otpremnica")
+	public List<StavkeOtpremnice> stavkeOtpremnice;
+	
+	@ManyToOne
+	public Narudzbenica narudzbenica;
+	
+	*/
 }
