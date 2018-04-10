c0f9166fd99ed75d2fed151e657a5d4711eaf976
Marko Matijevic <matijevic.markooo@gmail.com>
2018-04-10 19:09:04 +0200

    zadnji kontroleri PoslovnaGodina i PoslovniPartner. Cujemo se za nastavaka :)

---
+++ src/main/java/pi/controller/PoslovnaGodina.java
@@ -0,0 +1,67 @@
+package pi.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import pi.dto.PoslovnaGodinaDTO;
+import pi.repository.PoslovnaGodinaRepository;
+
+@RestController
+@RequestMapping(path="/PoslovnaGodina")
+public class PoslovnaGodina {
+	
+	@Autowired
+	private PoslovnaGodinaRepository poslovnaGodinaRepository;
+	
+	@GetMapping
+	public @ResponseBody List<PoslovnaGodinaDTO> readAll(){
+		List<pi.model.PoslovnaGodina> poslovnaGodina = (List<PoslovnaGodina>) poslovnaGodinaRepository.findAll();
+		List<PoslovnaGodinaDTO> poslovnaGodinaDTO = new ArrayList<>();
+		for (pi.model.PoslovnaGodina poslovnaGodina2 : poslovnaGodina){
+			poslovnaGodinaDTO.add(new PoslovnaGodinaDTO(poslovnaGodina2));
+		}
+		return poslovnaGodinaDTO;
+	}
+	@PostMapping
+	public @ResponseBody pi.model.PoslovnaGodina create (@RequestBody PoslovnaGodinaDTO dto){
+		pi.model.PoslovnaGodina psg = new pi.model.PoslovnaGodina();
+		psg.setGodina(dto.getGodina());
+		psg.setPreduzece(dto.getPreduzece());
+		return poslovnaGodinaRepository.save(psg);
+		
+	} 
+	@GetMapping("{/id}")
+	public @ResponseBody PoslovnaGodinaDTO readOne(@PathVariable(value = "id")Integer id){
+		pi.model.PoslovnaGodina psg = poslovnaGodinaRepository.findById(id).get();
+		return new PoslovnaGodinaDTO(psg);
+	}
+	
+	@PutMapping("{/id}")
+	public @ResponseBody PoslovnaGodinaDTO update(@PathVariable(value = "id")Integer id, @RequestBody PoslovnaGodinaDTO dto){
+		pi.model.PoslovnaGodina psg = new pi.model.PoslovnaGodina();
+		psg.setGodina(dto.getGodina());
+		psg.getPreduzece(dto.getPreduzece());
+		return new PoslovnaGodinaDTO(psg);
+	
+	}
+	
+	@DeleteMapping("{/id}")
+	public @ResponseBody PoslovnaGodinaDTO delete(@PathVariable(value = "id")Integer id){
+		pi.model.PoslovnaGodina psg = poslovnaGodinaRepository.findById(id).get();
+		poslovnaGodinaRepository.delete(psg);
+		return new PoslovnaGodinaDTO(psg);
+		
+	}
+
+}
---
+++ src/main/java/pi/controller/PoslovniPartner.java
@@ -0,0 +1,71 @@
+package pi.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import pi.dto.PoslovniPartnerDTO;
+import pi.repository.PoslovniPartnerRepository;
+
+@RestController
+@RequestMapping(path = "/PoslovniPartner")
+public class PoslovniPartner {
+	
+	@Autowired
+	private PoslovniPartnerRepository poslovniPartnerRepository;
+	
+	@GetMapping
+	public @ResponseBody List<PoslovniPartnerDTO> readAll(){
+		List<pi.model.PoslovniPartner> poslovniPartner = (List<PoslovniPartner>) poslovniPartnerRepository.findAll();
+		List<PoslovniPartnerDTO> poslovniPartnerDTO = new ArrayList<>();
+		for (pi.model.PoslovniPartner poslovniPartner2 : poslovniPartner2){
+			poslovniPartnerDTO.add(new PoslovniPartnerDTO(poslovniPartner2));
+		}
+		return poslovniPartnerDTO;
+	}
+	
+	@PostMapping
+	public @ResponseBody pi.model.PoslovniPartner create (@RequestBody PoslovniPartnerDTO dto){
+		pi.model.PoslovniPartner pp = new pi.model.PoslovniPartner();
+		pp.setAdresa(dto.getAdresa());
+		pp.setNazivPartnera(dto.getNazivPartnera());
+		pp.setPreduzece(dto.getPreduzece());
+		pp.setVrstaPartnera(dto.getVrstaPartnera());
+		return poslovniPartnerRepository.save(pp);
+	}
+	
+	@GetMapping("{/id}")
+	public @ResponseBody PoslovniPartnerDTO readOne(@PathVariable(value = "id")Integer id){
+		pi.model.PoslovniPartner pp = poslovniPartnerRepository.findById(id).get();
+		return new PoslovniPartnerDTO(pp);
+	}
+	
+	@PutMapping
+	public @ResponseBody PoslovniPartnerDTO update(@PathVariable(value = "id")Integer id, @RequestBody PoslovniPartnerDTO dto){
+		pi.model.PoslovniPartner pp = poslovniPartnerRepository.findById(id).get();
+		pp.setAdresa(dto.getAdresa());
+		pp.setNazivPartnera(dto.getNazivPartnera());
+		pp.setPreduzece(dto.getPreduzece());
+		pp.setVrstaPartnera(dto.getVrstaPartnera());
+		poslovniPartnerRepository.save(pp);
+		return new PoslovniPartnerDTO(pp);
+	}
+	
+	@DeleteMapping("{/id}")
+	public @ResponseBody PoslovniPartnerDTO delete (@PathVariable(value = "id")Integer id){
+		pi.model.PoslovniPartner pp = poslovniPartnerRepository.findById(id).get();
+		poslovniPartnerRepository.delete(pp);
+		return new PoslovniPartnerDTO(pp);
+	}
+
+}
