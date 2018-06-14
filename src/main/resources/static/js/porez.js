faad9060c6264cb4d83f7b5727db8d28ac71a79a
Marko Matijevic <matijevic.markooo@gmail.com>
2018-06-14 21:17:37 +0200

    Kolege ja sam odradio svoj deo html.
    Pozdrav cujemo se za ostatak!!!!!

---
+++ src/main/resources/static/js/porez.js
@@ -0,0 +1,170 @@
+function highlightRow(row) {
+	// ne reagujemo na klik na header tabele, samo obicne redove
+	// this sadrzi red na koji se kliknulo
+	if (!$(row).hasClass("header")) {
+		// klasa highlighted postavlja pozadinu na plavu
+		// njenim dodavanjem ili uklanjanjem oznacavamo da neki red
+		// dobija, odnosno gubi selekciju
+		// uklanjamo sa trenutno selektovanog
+		$(".highlighted").removeClass("highlighted");
+		// dodajemo na novi selektovani
+		$(row).addClass("highlighted");
+		// pozivamo sinhronizaciju, prosledjujemo dati red
+		sync($(row));
+	}
+}
+
+function sync(item) {
+	nazivPoreza = item.find(".nazivPoreza").html()
+	id = item.find(".id").html()
+	vazeci = item.find(".vazeci").html()
+	$("#nazivPoreza").val(nazivPoreza);
+	$("#vazeci").val(vazeci);
+
+}
+
+$(document).on("click", "tr", function(event) {
+	highlightRow(this)
+});
+
+$(document).on("click", ".remove", function(event){
+	//ne salji get zahtev
+	event.preventDefault(); 
+	url = $(this).attr("href")
+	//red koji se treba izbrisati ako je brisanje na serveru bilo uspesno
+	tr_parent = $(this).closest("tr")
+	$.ajax({
+    	url: url,
+    	type: "DELETE",
+    	success: function(){
+    		//ukloni i na strani 
+			tr_parent.remove()
+        }
+	});
+});
+
+$(document).ready(function() {
+	$("#porezPickup").click(function() {
+		id = $(".highlighted").find(".id").html();
+		$("select").val(id);
+		$('#porezModal').modal('toggle');
+	});
+	
+	$("#first").click(function(){
+		goFirst();
+	 });
+	
+	$("#last").click(function(){
+		goLast();
+	 });
+	
+	$("#next").click(function(){
+		goNext();
+	 });
+	
+	$("#prev").click(function(){
+		goPrevious();
+	 });
+
+	console.log("Krece ajax");
+	$.ajax({
+		url : "http://localhost:8080/Porez/"})
+		.then(
+				function(data) {
+					console.log("Uspeo")
+					for (i = 0; i < data.length; i++) {
+						var newRow = "<tr>"
+						+ "<td class=\"nazivPorez\">"
+						+ data[i].nazivPorez
+						+ "</td>"
+						+ "<td class=\"vazeci\">"
+						+ data[i].vazeci
+						+ "</td>"
+						+ "<td><a class=\"remove\" href='/Porez/" + data[i].id + "'>"
+						+ "<img src='images/remove.gif'/></a></td>"
+						+ "<td style=\"visibility: hidden; max-width: 0px;\" class=\"id\">"
+						+ data[i].id + "</td>"
+						$("#dataTable").append(newRow)
+					}
+				});
+	
+	/*$.ajax({
+		url : "http://localhost:8080/Porez/"})
+		.then(
+				function(data) {
+					console.log("Uspeo")
+					for (i = 0; i < data.length; i++) {
+						var newOption = '<option value="' + data[i].PIB + '">'
+						+ data[i].naziv + '</option>';
+						$("#preduzece").append(newOption);
+					}
+				});
+	*/
+	
+	
+	$("#add").click(function(){
+		// pripremamo JSON koji cemo poslati
+			console.log("start");
+			formData = JSON.stringify({
+	            nazivPoreza : $("#inputForm [name='nazivPoreza']").val(),
+	            vazeci :$("#inputForm [name='vazeci']").isSelected(),
+	        });
+			console.log(formData);
+			$.ajax({
+				url: "http://localhost:8080/Porez",
+				type: "POST",
+				data: formData,
+				// saljemo json i ocekujemo json nazad
+				contentType: "application/json",
+				datatype: 'json',
+				success: function(data) {
+					var newRow = "<tr>"
+					+ "<td class=\"nazivPoreza\">"
+					+ data.nazivPoreza
+					+ "</td>"
+					+ "<td class=\"vazeci\">"
+					+ data.vazeci
+					+ "</td>"
+					+ "<td><a class=\"remove\" href='/Porez/" + data.id + "'>"
+					+ "<img src='images/remove.gif'/></a></td>"
+					+ +"<td class=\"idCell\">"
+					+ data.id + "</td>"
+					"</tr>"
+					$("#dataTable").append(newRow)
+				  }
+				});
+			$('#inputModal').modal('toggle');
+			console.log("end");
+	 });
+	
+	$("#potvrda").on("click", function(event){
+		event.preventDefault();
+		console.log("Kliknuta potvrda");
+		var formData = JSON.stringify({
+			id : $("#editForm [name='cenovnikId']").val(),
+            nazivPoreza : $("#editForm [name='nazivPoreza']").val(),
+            vazeci :$("#editForm [name='vazeci']").isSelected(),
+        });
+		console.log(formData);
+		$.ajax({
+			url: "http://localhost:8080/Porez/" + $("#editForm [name='id']").val(),
+			type: "PUT",
+			data: formData,
+			// saljemo json i ocekujemo json nazad
+			contentType: "application/json",
+			datatype: 'json',
+			success: function(data) {
+				$(".highlighted").find(".nazivPoreza")[0].innerHTML = data.nazivPoreza;
+				$(".highlighted").find(".vazeci")[0].isSelected()= data.vazeci;
+			  },
+			error: function() {
+				console.log("Nije updateovao!")
+			}
+			});
+	});
+	
+	$("#rollback").click(function(event){
+		event.preventDefault();
+		sync($(".highlighted"));
+	});
+});
